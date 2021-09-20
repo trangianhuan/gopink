82074601bdecdb3cdb9fbb8b51fe5c0b3493db73
tran.gia.nhuan <tran.gia.nhuan@sun-asterisk.com>
2021-09-20 23:37:27 +0700

    Updates

---
+++ component---src-pages-about-js-d3272f92ba508d7088bc.js
@@ -0,0 +1,2 @@
+(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[682],{5518:function(e,t,l){"use strict";l.r(t);var n=l(7294),u=l(5444);t.default=function(){return n.createElement("main",null,n.createElement("title",null,"About Me"),n.createElement(u.rU,{to:"/"},"Home"),n.createElement("h1",null,"About Me"),n.createElement("p",null,"Hi there! I'm the proud creator of this site, which I built with Gatsby."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-d3272f92ba508d7088bc.js.map
